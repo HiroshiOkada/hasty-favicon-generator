1de01d951f2af553bbba58ac3273407937f00266
Hiroshi Okada <okadahiroshi@miobox.jp>
2017-12-08 15:40:40 +0900

    Redo styles

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Hasty favicon generator</h1>
         <Canvas size={180} id="Touch180" />
         <Canvas size={64} id="Favicon64" />
         <Canvas size={32} id="Favicon32" />
         <Canvas size={24} id="Favicon24" />
         <Canvas size={16} id="Favicon16" />
         <Caption />
+        <Download />
         <ColorSelector target={TEXT_COLOR} />
         <ColorSelector target={FILL_COLOR} />
-        <Download />
       </div>
     )
   }
